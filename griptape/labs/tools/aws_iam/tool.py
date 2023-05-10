c83cc177f6abee69deacb9da5d3861a4f6b468ba
Kyle Roche <kyro@griptape.ai>
2023-05-10 14:33:08 -0700

    Stub for session / iam cred tool
    goal is "who am i" and profile switching per https://ben11kehoe.medium.com/boto3-sessions-and-why-you-should-use-them-9b094eb5ca8e

---
+++ griptape/labs/tools/aws_iam/tool.py
@@ -0,0 +1,24 @@
+import json
+import logging
+from griptape.artifacts import BaseArtifact, TextArtifact, ErrorArtifact
+from griptape.core import BaseTool
+from griptape.core.decorators import activity
+from schema import Schema, Literal
+from attr import define, field
+import boto3
+
+@define
+class AwsPricing(BaseTool):
+    aws_access_key_id: str = field(default=None, kw_only=True, metadata={"env": "AWS_ACCESS_KEY_ID"})
+    aws_secret_access_key: str = field(default=None, kw_only=True, metadata={"env": "AWS_SECRET_ACCESS_KEY"})
+
+    @activity(config={
+        "name": "get_caller_identity",
+        "description": "can be used to get the account or IAM principal of the caller",
+        "schema": Schema(
+            str,
+            description="context of this activity"
+        )
+    })
+    def get_caller_identity(self, value: bytes) -> BaseArtifact:
+        return BaseArtifact("not implemented")
