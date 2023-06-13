0f2b673ec7c3076d1397d43e4be69d862db12d12
Vasily Vasinov <des.elyon@gmail.com>
2023-06-13 16:39:18 -0600

    Update the ProxycurlClient tool to include custom profile endpoint params and split the output into a ListArtifact

--- griptape/labs/tools/proxycurl_client/tool.py
+++ griptape/labs/tools/proxycurl_client/tool.py
@@ -1,4 +1,4 @@
-from griptape.artifacts import BaseArtifact, TextArtifact, ErrorArtifact
+from griptape.artifacts import BaseArtifact, TextArtifact, ErrorArtifact, ListArtifact
 from griptape.core import BaseTool
 from griptape.core.decorators import activity
 from schema import Schema, Literal
@@ -8,12 +8,10 @@ import requests
 
 @define
 class ProxycurlClient(BaseTool):
-    api_key: str = field(kw_only=True)
+    GET_PROFILE_ENDPOINT = "https://nubela.co/proxycurl/api/v2/linkedin"
 
-    api_endpoint: str = field(
-        default="https://nubela.co/proxycurl/api/v2/linkedin",
-        kw_only=True
-    )
+    api_key: str = field(kw_only=True)
+    get_profile_params: dict = field(factory=dict, kw_only=True)
 
     timeout = field(default=30, kw_only=True)
 
@@ -31,16 +29,19 @@ class ProxycurlClient(BaseTool):
     def get_profile(self, params: dict) -> BaseArtifact:
         profile_id = params["values"]["profile_id"]
         headers = {"Authorization": f"Bearer {self.api_key}"}
-        params = {
-            "url": f"https://www.linkedin.com/in/{profile_id}",
-        }
+        params = self.get_profile_params
+
+        params["url"] = f"https://www.linkedin.com/in/{profile_id}"
 
         response = requests.get(
-            self.api_endpoint, params=params, headers=headers, timeout=self.timeout
+            self.GET_PROFILE_ENDPOINT, params=params, headers=headers, timeout=self.timeout
         )
 
         if response.status_code == 200:
-            return TextArtifact(response.text)
+            return ListArtifact.from_list([
+                TextArtifact(str({key: value}))
+                for key, value in response.json().items()
+            ])
         else:
             return ErrorArtifact(
                 f"Proxycurl returned an error with status code "
